d97b91afba3dd04822fac9b8db59553e7312668b
rohanrrj <66823845+rohanrrj@users.noreply.github.com>
2022-07-05 21:44:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// Titles: https://omdbapi.com/?s=thor&page=1&apikey=fc1fef96
-// details: http://www.omdbapi.com/?i=tt3896198&apikey=fc1fef96
+
 
 const movieSearchBox = document.getElementById('movie-search-box');
 const searchList = document.getElementById('search-list');
